95d8daaf48e8bb22dfb3e16839e16d34a3e55a3f
antoine <antoineabonneau94@gmail.com>
2022-04-30 12:43:18 -0400

    Added spaces for visibility

--- index.php
+++ index.php
@@ -11,12 +11,14 @@
         <?php require_once('./addDropdownMenu.php'); ?>
 
         <h1>Administration Page</h1>
+
         <form action="updateDatabase.php" method="POST">
             <div class="update_form">
                 <div class="first_row">
                     <select name="armoire_id" id="armoire_id"> <?php addMenu(10); ?> </select>
                     <select name="floor_id" id="floor_id"> <?php include addMenu(3); ?> </select>
                 </div>
+
                 <div class="second_row">
                     <select name="area_id" id="area_id"> <?php include addMenu(4); ?> </select>
                     <select name="sensor_type" id="sensor_type">
@@ -24,16 +26,18 @@
                         <option value="distance"> Capteur de distance </option>
                     </select>
                 </div>
+
                 <div class="third_row">
                     <input name="component" id="component" placeholder="Composant">
                     <input name="restock_quantity" id="restock_quantity" placeholder="Quantité de restock">
                 </div>
+                
                 <div class="update">
                     <button>Mettre à jour la BDD</button>
                 </div>
             </div>
         </form>
 
-        <script src="autoFillForm.js"></script>
+        <script src="scripts/autoFillForm.js"></script>
     </body>
 </html>
