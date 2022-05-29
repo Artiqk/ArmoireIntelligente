53ee8cdb759f1ada3d84ee6ee2e77a75c46e2675
antoine <antoineabonneau94@gmail.com>
2022-05-29 09:38:20 -0400

    Added and styled labels for inputs

--- index.php
+++ index.php
@@ -15,12 +15,18 @@
         <form action="updateDatabase.php" method="POST">
             <div class="update_form">
                 <div class="first_row">
-                    <select name="armoire_id" id="armoire_id"> <?php addMenu(10); ?> </select> <!-- FIXME - DRY Add to PHP function -->
-                    <select name="floor_id" id="floor_id"> <?php include addMenu(3); ?> </select>
+                    <label for="armoire_id"> N° de l'armoire </label>
+                    <input name="armoire_id" id="armoire_id" placeholder="N° de l'armoire">
+
+                    <label for="floor_id"> N° de l'étage </label>
+                    <input name="floor_id" id="floor_id" placeholder="N° de l'étage">
                 </div>
 
                 <div class="second_row">
-                    <select name="area_id" id="area_id"> <?php include addMenu(4); ?> </select>
+                    <label for="area_id"> N° de l'emplacement </label>
+                    <input name="area_id" id="area_id" placeholder="N° de l'emplacement">
+
+                    <label for="sensor_type"> Type de capteur </label>
                     <select name="sensor_type" id="sensor_type">
                         <option value="force"> Capteur de force </option>
                         <option value="distance"> Capteur de distance </option>
@@ -28,8 +34,11 @@
                 </div>
 
                 <div class="third_row">
+                    <label for="componenet"> Composant </label>
                     <input name="component" id="component" placeholder="Composant">
-                    <input name="restock_quantity" id="restock_quantity" placeholder="Quantité de restock">
+
+                    <label for="restock_quantity"> Seuil </label>
+                    <input name="restock_quantity" id="restock_quantity" placeholder="Seuil">
                 </div>
                 
                 <div class="update">
